16e7815131764ac1378f3c4dea209309d0bb5e56
d-sofiika <soniadrozd2709@gmail.com>
2024-01-29 14:44:58 +0100

    added console.log

--- js/task-3.js
+++ js/task-3.js
@@ -6,8 +6,8 @@ function getElementWidth(content, padding, border){
 return content + padding * 2 + border * 2
 };
 
-getElementWidth("50px", "8px", "4px");
-getElementWidth("60px", "12px", "8.5px");
-getElementWidth("200px", "0px", "0px");
+console.log (getElementWidth("50px", "8px", "4px"));
+console.log (getElementWidth("60px", "12px", "8.5px"));
+console.log (getElementWidth("200px", "0px", "0px"));
 
 
